6a6213c53265af0f246affcb06524d355f371bf5
Mia Moon <miamoon1121@gmail.com>
2022-12-17 14:09:05 -0500

    Progress

--- components/display/GroceryItem.tsx
+++ components/display/GroceryItem.tsx
@@ -1,14 +1,33 @@
-import { Box, Typography } from "@mui/material";
+import { Box, IconButton, Typography } from "@mui/material";
+import { deleteDoc, doc } from "firebase/firestore";
+import { db } from "../../util/firebase";
+import DeleteIcon from "@mui/icons-material/Delete";
 import { GroceryWithId } from "../../types";
 
 type Props = {
-  readonly groceryName: string;
+  readonly grocery: GroceryWithId;
 };
 
-const GroceryItem = ({ groceryName }: Props) => {
+const GroceryItem = ({ grocery: { name, amount, id } }: Props) => {
+  const specificDocRef = doc(db, "groceries", id);
+
+  const deleteTask = async () => {
+    await deleteDoc(specificDocRef);
+  };
+
   return (
-    <Box>
-      <Typography>{groceryName}</Typography>
+    <Box
+      sx={{
+        margin: 1,
+        display: "flex",
+        gap: 3,
+      }}
+    >
+      <Typography>{name}</Typography>
+      <Typography>{amount}</Typography>
+      <IconButton size="small" onClick={deleteTask}>
+        <DeleteIcon />
+      </IconButton>
     </Box>
   );
 };
